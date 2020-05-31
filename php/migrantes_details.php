26da921b016300bea63300a242b88ca71e43877d
mauguzz <mauricio.guzz@outlook.com>
2020-05-30 22:17:21 -0500

    array

--- php/migrantes_details.php
+++ php/migrantes_details.php
@@ -9,9 +9,9 @@ require_once('DataBase.php');
 
 //$result=DataBase::Mostrar_Migrante_Detalles($id);
 
-$result=["datos generales"=> ["Nombre"=>"Mauricio", "Edad"=>"20", "Punto"=> "2"], 
+$result=array(["datos generales"=> ["Nombre"=>"Mauricio", "Edad"=>"20", "Punto"=> "2"], 
         "trabajos"=>["Fecha"=>"30 de mayo de 2020","Detalles"=>"Backend Developer","Requisitos"=>"PHP","Direccion"=>"Jiutepec"] , 
-        "actividades_culturales"=>["Fecha"=>"23 de mayo de 2020", "Nombre"=> "Guitarra", "Detalles"=>"Clases básicas", "Requisitos"=>"Ninguno", "Activo"=>"Si"]];
+        "actividades_culturales"=>["Fecha"=>"23 de mayo de 2020", "Nombre"=> "Guitarra", "Detalles"=>"Clases básicas", "Requisitos"=>"Ninguno", "Activo"=>"Si"]]);
 echo json_encode(($result),JSON_UNESCAPED_UNICODE);
 
 ?>
