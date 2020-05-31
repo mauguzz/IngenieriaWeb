60991aeb3e806076f0747c5b787cb9034559dc71
mauguzz <mauricio.guzz@outlook.com>
2020-05-30 22:06:07 -0500

    s

--- php/migrantes_details.php
+++ php/migrantes_details.php
@@ -3,9 +3,9 @@
 require_once('DataBase.php');
 
 //Obtención de datos de la solicitud
-$json=file_get_contents('php://input');
-$data=json_decode($json);
-$id=$data->id;
+//$json=file_get_contents('php://input');
+//$data=json_decode($json);
+//$id=$data->id;
 
 //$result=DataBase::Mostrar_Migrante_Detalles($id);
 
