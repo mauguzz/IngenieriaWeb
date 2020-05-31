4855eed1f16315216e7c52b2681f78345f93fc69
mauguzz <mauricio.guzz@outlook.com>
2020-05-30 21:09:40 -0500

    Añadido un JSON de ejemplo desde Backend

--- php/migrantes_details.php
+++ php/migrantes_details.php
@@ -7,7 +7,11 @@ $json=file_get_contents('php://input');
 $data=json_decode($json);
 $id=$data->id;
 
-$result=DataBase::Mostrar_Migrante_Detalles($id);
+//$result=DataBase::Mostrar_Migrante_Detalles($id);
+
+$result=["datos generales"=> ["Nombre"=>"Mauricio", "Edad"=>"20", "Punto"=> "2"], 
+        "trabajos"=>["Fecha"=>"30 de mayo de 2020","Detalles"=>"Backend Developer","Requisitos"=>"PHP","Direccion"=>"Jiutepec"] , 
+        "actividades_culturales"=>["Fecha"=>"23 de mayo de 2020", "Nombre"=> "Guitarra", "Detalles"=>"Clases básicas", "Requisitos"=>"Ninguno", "Activo"=>"Si"]];
 echo json_encode(($result),JSON_UNESCAPED_UNICODE);
 
 ?>
