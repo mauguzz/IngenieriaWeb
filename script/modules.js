2afb97214a8c68e802bbd53e5caad2bda366e441
mauguzz <mauricio.guzz@outlook.com>
2020-06-04 14:05:28 -0500

    JS

--- script/modules.js
+++ script/modules.js
@@ -15,7 +15,7 @@ function table_consultar_todos(uri, tbody, columnformat){
     .then(res=>res.json())
     .then(res_json=>{
         console.log(res_json);
-        let rows=res_json[0];
+        let rows=res_json['migrantes'];  //Cambiar en su momento
         console.log(rows);
 
         tbody.innerHTML="";
