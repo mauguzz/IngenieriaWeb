308ae72a775766061ee1d65169ec218fecafddb1
mauguzz <mauricio.guzz@outlook.com>
2020-06-03 22:29:43 -0500

    PHP

--- php/res_migrantes.php
+++ php/res_migrantes.php
@@ -7,10 +7,10 @@ $uri=$_SERVER['REQUEST_URI'];       //Capturar URI utilizada
 
 
 //MÉTODO HTTP GET
+/*
 function res_get($uri){
+    
           
-    //if($uri[0] == 'res_migrantes.php'){
-        //array_shift($uri);
         if($uri){
             if($uri[0]==""){ //Si no se especifica un id pero si se puso un slash
                 header('HTTP/1.1 400 Bad Request');
@@ -25,12 +25,29 @@ function res_get($uri){
             $result=array("migrantes"=>array("1"=>array("Nombre"=>"Mauricio", "Apellido_Paterno"=>"Gutiérrez", "Apellido_Materno"=>"Montor", "Ciudad"=>"Jiutepec"), 
             "2"=>array("Nombre"=>"Fabián", "Apellido_Paterno"=>"Sánchez", "Apellido_Materno"=>"Moreno", "Ciudad"=>"Ecatepec")));
         }
-            
-    //} else {
-        // Sólo se aceptan resources desde 'clients'
-      //  header('HTTP/1.1 404 Not Found');
-    //}
+
     return $result;
+}
+*/
+
+function res_get(){
+    //USO: Pasar un único argumento entero id si se va a aplicar la acción a un elemento específico
+    //Si no, no es necesario pasar argumentos.
+    $args=func_get_args();
+
+    if (count($args)==1){
+        //Se pasó el id. //CASO: Obtener detalles de un migrante
+        $result=array("id"=>$args[0],"datos_generales"=> array("Nombre"=>"Mauricio", "Edad"=>"20", "Punto"=> "2"), 
+            "trabajos"=>array("1"=> array("Fecha"=>"30 de mayo de 2020","Detalles"=>"Backend Developer","Requisitos"=>"PHP","Direccion"=>"Jiutepec") ), 
+            "actividades_culturales"=>array("1"=> array("Fecha"=>"23 de mayo de 2020", "Nombre"=> "Guitarra", "Detalles"=>"Clases básicas", "Requisitos"=>"Ninguno", "Activo"=>"Si")));
+    }else{
+        //No se pasó el id. //CASO: Obtener todos los migrantes
+        $result=array("migrantes"=>array("1"=>array("Nombre"=>"Mauricio", "Apellido_Paterno"=>"Gutiérrez", "Apellido_Materno"=>"Montor", "Ciudad"=>"Jiutepec"), 
+        "2"=>array("Nombre"=>"Fabián", "Apellido_Paterno"=>"Sánchez", "Apellido_Materno"=>"Moreno", "Ciudad"=>"Ecatepec")));
+    }
+          
+
+return $result;
 }
 
 //MÉTODO HTTP POST
@@ -54,22 +71,30 @@ $uri=explode("/",$uri);
 $uri=array_slice($uri,3); //Elimina las primeras tres partes irrelevantes de la uri (""/"projectfolder"/"php")
 if($uri[0] == 'res_migrantes.php'){
     array_shift($uri);  //Eliminar después
-    /*
+    
+
+
+
     if($uri=array_slice($uri,1)){
         if($uri[0]==""){ //Si no se especifica un id pero si se puso un slash
             header('HTTP/1.1 400 Bad Request');
+            return;
         } else { //Si se especifica un id
+            $id=$uri[0];
+            $id_specified=TRUE;
             //CASO: Obtener detalles de un migrante
             
         }
     }else{
         //CASO: Obtener todos los migrantes
-       
+       $id_specified=FALSE;
     }
-    */
+    
+
+
     switch($method){
         case 'GET':
-                $result=res_get($uri);
+                $id_specified ? ($result=res_get($id)) : ($result=res_get());
         break;
         case 'POST':
                 res_post($uri);
