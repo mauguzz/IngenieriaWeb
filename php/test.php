bf610eb78677bd07b6d0e23e4ec2613cb0cb078e
mauguzz <mauricio.guzz@outlook.com>
2020-06-04 21:49:30 -0500

    JS

--- php/test.php
+++ php/test.php
@@ -5,7 +5,7 @@ require_once('DataBase.php');
 
 
 $Conexion= new Database();
-$result=$Conexion->Mostrar_Migrante_Detalle($Conexion, $id);
+$result=$Conexion->Mostrar_Migrante_Detalle($Conexion, 1);
 
 //foreach($result)
 echo(json_encode($result));
