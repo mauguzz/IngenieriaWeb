47b90636ac6033af4825be56133a58df9924859c
mauguzz <mauricio.guzz@outlook.com>
2020-06-04 21:54:13 -0500

    JS

--- php/test.php
+++ php/test.php
@@ -1,13 +1,23 @@
 <?php
-require_once('DataBase.php');
 
-
-
+include('DataBase.php');
 
 $Conexion= new Database();
-$result=$Conexion->Mostrar_Migrante_Detalle($Conexion, 1);
+$result=$Conexion->Mostrar_Migrantes_Todos($Conexion);
 
-//foreach($result)
-echo(json_encode($result));
+
+
+foreach($result as $Fila){
+
+<tr>
+<td><?php echo $Fila['NoBoleta']?></td>
+<td><?php echo $Fila['Nombre']?></td>
+<td><?php echo $Fila['APaterno']?></td>
+<td><?php echo $Fila['AMaterno']?></td> 
+<td><?php echo $Fila['NoEmpleado']?></td> 
+<td><?php echo $Fila['State']?></td> 
+</tr>
+
+
 
 ?>
