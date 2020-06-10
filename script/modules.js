e120432ee35ad0b3545c11caae455a1dfbab590b
mauguzz <mauricio.guzz@outlook.com>
2020-06-09 22:32:15 -0500

    JS

--- script/modules.js
+++ script/modules.js
@@ -72,7 +72,7 @@ function table_consultar_todos(uri, thead, tbody, columns, rowsindex){
             console.log(result);
             data.push(result);
         })
-        console.log(data);
+        console.log(data.text());
         
 
 
