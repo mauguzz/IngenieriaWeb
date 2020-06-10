5aa02374c3603cbe4bed0f1e673c8c60abc368be
mauguzz <mauricio.guzz@outlook.com>
2020-06-09 20:20:03 -0500

    Cargar tabla al abrir pagina

--- script/migrantes.js
+++ script/migrantes.js
@@ -60,6 +60,25 @@ document.addEventListener("DOMContentLoaded", function(event) {
     
 });
 
+function filter(tableReg, searchText) {
+    
+    searchText.value.toLowerCase();
+    for (var i = 1; i < tableReg.rows.length; i++) {
+        var cellsOfRow = tableReg.rows[i].getElementsByTagName('td');
+        var found = false;
+        for (var j = 0; j < cellsOfRow.length && !found; j++) {
+            var compareWith = cellsOfRow[j].innerHTML.toLowerCase();
+            if (searchText.length == 0 || (compareWith.indexOf(searchText) > -1)) {
+                found = true;
+            }
+        }
+        if (found) {
+            tableReg.rows[i].style.display = '';
+        } else {
+            tableReg.rows[i].style.display = 'none';
+        }
+    }
+}
 
 btn_migrante_eliminar.addEventListener("click", ()=>{
     migrante_eliminar(id);
