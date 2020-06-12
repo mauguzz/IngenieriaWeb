4ebccb2fa8ef0da52db0ee4321048e6cbe789c0e
mauguzz <mauricio.guzz@outlook.com>
2020-06-12 00:39:59 -0500

    JS

--- script/migrantes.js
+++ script/migrantes.js
@@ -6,8 +6,12 @@ const mcontent = document.getElementById('content'); //Div del contenido princip
 
 
 const t_migrantes = document.getElementById('t_migrantes');
-const t_registros = document.getElementById('t_registros')
+const t_general = document.getElementById('t_general');
+const t_culturales = document.getElementById('t_culturales');
+const t_laborales = document.getElementById('t_laborales');
+const t_registros = document.getElementById('t_registros');
 
+/*
 const thead_general = document.getElementById('thead_general');
 const thead_culturales = document.getElementById('thead_culturales');
 const thead_laborales = document.getElementById('thead_laborales');
@@ -19,6 +23,7 @@ const tbody_culturales = document.getElementById('tbody_culturales');
 const tbody_laborales = document.getElementById('tbody_laborales');
 const tbody_migrantes = document.getElementById('tbody_migrantes');
 const tbody_registros = document.getElementById('tbody_registros');
+*/
 
 const btn_consultar =document.getElementById('btn_consultar');
 const btn_migrante_eliminar = document.getElementById('btn_migrante_eliminar');
@@ -32,15 +37,15 @@ console.log(t_registros.children['thead_registros'])
 
 btn_consultar.addEventListener("click", ()=>{ 
     migrante_consultar(id,
-        thead_general, 
-        thead_culturales, 
-        thead_laborales, 
+        t_general.children['thead_general'], 
+        t_culturales.children['thead_culturales'], 
+        t_laborales.children['thead_laborales'], 
         //thead_registros, 
         t_registros.children['thead_registros'],
 
-        tbody_general,
-        tbody_culturales,
-        tbody_laborales, 
+        t_general.children['tbody_general'],
+        t_culturales.children['tbody_culturales'],
+        t_laborales.children['tbody_laborales'], 
         //tbody_registros
         t_registros.children['tbody_registros']
     );
