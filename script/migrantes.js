0781e507e67ff1beacb479999007fc1dd8444034
mauguzz <mauricio.guzz@outlook.com>
2020-06-12 00:43:10 -0500

    JS

--- script/migrantes.js
+++ script/migrantes.js
@@ -11,19 +11,6 @@ const t_culturales = document.getElementById('t_culturales');
 const t_laborales = document.getElementById('t_laborales');
 const t_registros = document.getElementById('t_registros');
 
-/*
-const thead_general = document.getElementById('thead_general');
-const thead_culturales = document.getElementById('thead_culturales');
-const thead_laborales = document.getElementById('thead_laborales');
-const thead_migrantes = document.getElementById('thead_migrantes');
-const thead_registros = document.getElementById('thead_registros')
-
-const tbody_general= document.getElementById('tbody_general');
-const tbody_culturales = document.getElementById('tbody_culturales');
-const tbody_laborales = document.getElementById('tbody_laborales');
-const tbody_migrantes = document.getElementById('tbody_migrantes');
-const tbody_registros = document.getElementById('tbody_registros');
-*/
 
 const btn_consultar =document.getElementById('btn_consultar');
 const btn_migrante_eliminar = document.getElementById('btn_migrante_eliminar');
@@ -37,16 +24,13 @@ console.log(t_registros.children['thead_registros'])
 
 btn_consultar.addEventListener("click", ()=>{ 
     migrante_consultar(id,
-        t_general.children['thead_general'], 
-        t_culturales.children['thead_culturales'], 
-        t_laborales.children['thead_laborales'], 
-        //thead_registros, 
-        t_registros.children['thead_registros'],
-
+        t_general.children['thead'], 
+        t_culturales.children['thead'], 
+        t_laborales.children['thead'], 
+        t_registros.children['thead'],
         t_general.children['tbody_general'],
         t_culturales.children['tbody_culturales'],
         t_laborales.children['tbody_laborales'], 
-        //tbody_registros
         t_registros.children['tbody_registros']
     );
 });
