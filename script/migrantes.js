26c830197eb0c66a297a5a7df6b159a12898619d
mauguzz <mauricio.guzz@outlook.com>
2020-06-13 20:08:32 -0500

    Limpieza de formulario y boton submit en ventana modal

--- script/migrantes.js
+++ script/migrantes.js
@@ -108,7 +108,7 @@ form_migrantes.onsubmit = function(e){
     e.preventDefault();
 
     //document.getElementById('modal_migrantes_form').hidde();
-    $('#modal_migrantes_form').hidde();
+    $('#modal_migrantes_form').hide();
     
     let formData = new FormData(form_migrantes);
     let formJson = JSON.stringify(Object.fromEntries(formData));
