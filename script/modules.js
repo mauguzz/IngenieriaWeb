29ef0f3f03134320d660fc79ef52eda2e18644ac
mauguzz <mauricio.guzz@outlook.com>
2020-06-15 18:30:54 -0500

    Detalles de datatables

--- script/modules.js
+++ script/modules.js
@@ -89,6 +89,7 @@ function table_generate_datatables(tablename, init, rows, cols){
             console.log("Caso sin rows")
             datatable = $(tablename).DataTable( {
                 select: true,
+                data: [],
                 columns: customCols,
                 dom: "frtip"
             }); 
