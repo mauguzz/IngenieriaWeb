59d8c85090872fec7c1e56aac0e5e02a89d51827
mauguzz <mauricio.guzz@outlook.com>
2020-06-15 19:23:22 -0500

    Detalles de datatables

--- script/modules.js
+++ script/modules.js
@@ -74,7 +74,7 @@ function table_generate_datatables(tablename, init, rows, cols){
         first=false;
     })
 
-    console.log(dataSet.length);
+    console.log(dataSet);
     
     let datatable;
     if(init){
