7e0c10aaa9557f253443ed321382524dc5afc209
mauguzz <mauricio.guzz@outlook.com>
2020-06-16 00:38:07 -0500

    Seguimiento de migrante

--- script/seguimiento.js
+++ script/seguimiento.js
@@ -41,7 +41,7 @@ import {migrante_consultar, migrante_consultar_todos, migrante_registrar, migran
                                         console.log("No especificado LLAVE");
                                         if(reason=="Llave no especificada"){
                                             console.log("No especificado LLAVE")
-                                            $('.close').click(); 
+                                            $('#close').click(); 
                                             $("[data-dismiss=modal]").trigger({ type: "click" }); 
                                             $('#modal_migrantes_details').on('shown', function() { 
                                                 $('#modal_migrantes_details').modal('hide'); 
