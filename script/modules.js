7ec9a4ceb4c5df00ae1b9ed97b537440441cbc21
mauguzz <mauricio.guzz@outlook.com>
2020-06-16 20:19:59 -0500

    Arreglando inicio de sesión

--- script/modules.js
+++ script/modules.js
@@ -633,7 +633,7 @@ export function Validar_Sesion(){
 export function Validar_SesionA(){
     fetch("php/res_sesion.php",{ method: 'GET'})
     .then(handleHttpErrors)
-    .then(res=>res.json())
+    .then(res=>res.text())
     .then(resjson=>{
         console.log(resjson);
         /*
