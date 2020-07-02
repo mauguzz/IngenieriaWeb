b730747611d9190dbba74b8d61946b6de4fabac1
mauguzz <mauricio.guzz@outlook.com>
2020-07-01 19:49:32 -0500

    Botones extra y diseño

--- script/migrantes.js
+++ script/migrantes.js
@@ -158,7 +158,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
                             },
                             action: ()=>{
                                 id=datatable.rows( { selected: true } ).data()[0][0]; 
-                                console.log(datatable.rows( { selected: true } ).data().select())
+                                console.log(datatable.rows( { selected: true } ).data().cell(0))
                                 edicion_migrante_culturales(id); //Llama a esta función para hacer consultas a la BD. Está implementada en este mismo archivo.
                             },   
                         },
