017ade12d1927e0b6557eaf0a5c1faeff4ed490b
mauguzz <mauricio.guzz@outlook.com>
2020-07-04 15:16:57 -0500

    Solucionado problema de inicio de sesion

--- script/modules.js
+++ script/modules.js
@@ -666,7 +666,7 @@ export function Iniciar_Sesion(formJson){
     fetch("php/res_sesion.php",{method: 'POST', body: formJson})   ///
     .then(handleHttpErrors)
     .then(response=>{
-        console.log(response);
+        console.log(response.text());
         console.log("F 2");
         if (response.redirected) {
             console.log("F 1");
