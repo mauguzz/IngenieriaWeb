2c5316eb0a1e642d4395664f771ea994534a0a19
mauguzz <mauricio.guzz@outlook.com>
2020-07-04 15:11:12 -0500

    Solucionado problema de inicio de sesion

--- php/DataBase.php
+++ php/DataBase.php
@@ -674,11 +674,16 @@ public static function Eliminar_Asistencia_Oferta_Laboral($mysqli, $ID_Migrante,
                         $_SESSION['POINTID']=$res[0]["Id_Punto_Control"];
 
                         //Agregado por Mau GTZ:
+                        try{
                         $Point=$res[0]["Id_Punto_Control"];
                         $ControlPointName=$Conexion->prepare("SELECT Nombre FROM Puntos_De_Control WHERE Id_Punto_Control='".$Point."'");
                         $ControlPointName=$ControlPointName->execute();
                         $ControlPointName=$ControlPointName-fetchAll(PDO::FETCH_ASSOC);
                         $_SESION['POINTNAME']=$ControlPointName[0]['Nombre'];
+                        }catch (Exception $e){
+                            $result=["error"=>e.getMessage()];
+                            return;
+                        }
 
                         header("location:http://localhost/IngenieriaWeb/migrantes.html");  
                         //header('Content-Type: text/html; charset=utf-8');
